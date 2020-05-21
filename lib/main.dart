c62ba2547b4663aa01998b4cdb87901d5be4fc6e
Akhil Ruhil <akhilruhil11@gmail.com>
2020-05-21 16:41:03 +0530

    A Xylophone app

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(Instrument());
+
+class Instrument extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade200,
+          title: Center(child: Text('Xylophone')),
+        ),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.blue, soundNumber: 7),
+              buildKey(color: Colors.green, soundNumber: 3),
+              buildKey(color: Colors.yellow, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.pinkAccent, soundNumber: 6),
+              buildKey(color: Colors.deepOrangeAccent, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
